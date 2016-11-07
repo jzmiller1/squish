32daf3eeb465d01023136c0a9b6ed84566067819
Zac Miller <zmiller1@gmail.com>
2016-11-07 15:15:58 -0500

    Add example import of census tabular data
    Created codebook loader for census tabular data
    
    Created loader for census 1990 county total population as an example for
    how to load all tabular data.

--- rundown.py
+++ rundown.py
@@ -79,22 +79,47 @@ for path, year in nlcd_data:
     cursor.execute(subprocess.check_output(raster2pgsql_command, shell=True))
     conn.commit()
 
-
+######
 ### Import Census
-# ### Load block group 1990
-loader('data\\census\\GA_blck_grp_1990_huc8.shp',
+######
+
+### Tabular ###
+cursor.execute("""CREATE TABLE codebook (code varchar(40), description varchar(280));""")
+
+f = open('data\\census\\tabular\\codebook.csv', 'r')
+cursor.copy_from(f, 'codebook', sep=',')
+f.close()
+
+### Load Population 1990 
+cursor.execute("""CREATE TABLE county_1990_pop
+                 (gisjoin varchar(40), year integer, county varchar(40), countya varchar(40), divisiona varchar(40),
+                  msa_cmsaa varchar(40), pmsaa varchar(40), regiona varchar(40), state varchar(40), statea varchar(40),
+                  anpsadpi varchar(60), et1001 integer)""")
+
+copy_sql = """
+           COPY county_1990_pop FROM stdin WITH CSV HEADER
+           DELIMITER as ','
+           """
+with open('data\\census\\tabular\\population\\1990\\GA_County_1990_Population.csv', 'r') as f:
+    cursor.copy_expert(sql=copy_sql, file=f)
+    conn.commit()
+
+### Spatial ###
+
+### Load block group 1990
+loader('data\\census\\spatial\\GA_blck_grp_1990_huc8.shp',
        "blckgroup_1990",
        '-c',
        102003)
 
 ### Load block group 2000
-loader('data\\census\\GA_blck_grp_2000_huc8.shp',
+loader('data\\census\\spatial\\GA_blck_grp_2000_huc8.shp',
        "blckgroup_2000",
        '-c',
        102003)
 
 ### Load block gp 2010
-loader('data\\census\\GA_blck_grp_2010_huc8.shp',
+loader('data\\census\\spatial\\GA_blck_grp_2010_huc8.shp',
        "blckgroup_2010",
        '-c',
        102003)
